f4ea751c365ddff7cce555984bdd133d4dc502e1
Mike Heft <mikeheft@gmail.com>
2024-06-17 10:32:55 -0600

    untype generated files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# typed: strict
+# typed: false
 
 class ApplicationController < ActionController::Base
 end
--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -1,3 +1,4 @@
+# typed: false
 Rails.autoloaders.each do |autoloader|
   autoloader.inflector = Zeitwerk::Inflector.new
   autoloader.inflector.inflect(
