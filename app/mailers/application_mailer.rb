558456f58fec7433c7cb36a4cec199579b5cbc6d
Mike Heft <mikeheft@gmail.com>
2024-06-17 09:40:41 -0600

    Fix issue with unregistered cop

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@
 # typed: false
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: "from@example.com"
+  layout "mailer"
 end
---
+++ lib/rubocop/init_autoloader.rb
@@ -0,0 +1,5 @@
+# typed: false
+# frozen_string_literal: true
+
+# Load all Ruby files in the custom directory
+Dir[File.join(__dir__, "**/*.rb")].each { |file| require file }
