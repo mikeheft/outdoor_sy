89b4cdc5750692d7f3b8a5c4373a25562b9327ad
Mike Heft <mikeheft@gmail.com>
2024-06-17 10:22:09 -0600

    Add initializer for zeitwerk rubocop inflector

---
+++ config/initializers/zeitwerk.rb
@@ -0,0 +1,7 @@
+Rails.autoloaders.each do |autoloader|
+  autoloader.inflector = Zeitwerk::Inflector.new
+  autoloader.inflector.inflect(
+    'rubocop' => 'RuboCop',
+    # other special cases
+  )
+end
