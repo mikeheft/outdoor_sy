d084d89784a52978b7d639dc6f85ae44ac0cc444
Mike Heft <mikeheft@gmail.com>
2024-06-18 09:32:12 -0600

    fix test after adding validations

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# typed: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_17_193345) do
   # These are extensions that must be enabled in order to support this database
-  rake_extension "plpgsql"
+  enable_extension "plpgsql"
 
   create_table "owners", force: :cascade do |t|
     t.string "first_name", null: false
--- spec/models/owners/model_spec.rb
+++ spec/models/owners/model_spec.rb
@@ -4,6 +4,7 @@
 require 'rails_helper'
 
 describe Owner do
+  subject {build(:owner)}
   it { is_expected.to validate_presence_of :first_name }
   it { is_expected.to validate_presence_of :last_name }
   it { is_expected.to validate_presence_of :email }
